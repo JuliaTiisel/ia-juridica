8a6e91b88dfc14a5bb1dda5ac62a62120c11c0e9
JuliaTiisel <juliatiisel@gmail.com>
2025-04-13 21:34:30 -0300

    ✅ Text

--- frontend/src/components/Billing.jsx
+++ frontend/src/components/Billing.jsx
@@ -8,28 +8,29 @@ const Billing = () => {
       <div className={layout.sectionImgReverse}>
         <img
           src={bill}
-          alt='billing'
+          alt='Documentos Jurídicos'
           className='w-[100%] h-[100%] relative z-[5]'
         />
         <div className='absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white__gradient'/>
         <div className='absolute z-[0] -left-1/2 bottom-0 w-[50%] h-[50%] rounded-full pink__gradient'/>
       </div>
       <div className={layout.sectionInfo}>
-        <h2 className={styles.heading2}>Easily control your <br className='sm:block hidden'/> billing & invoicing.</h2>
+        <h2 className={styles.heading2}>
+          Controle facilmente suas <br className='sm:block hidden'/> petições e análises jurídicas.
+        </h2>
         <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
-          Elit enim sed massa etiam.
-          Mauris eu adipiscing ultrices ametodio aenean neque.
-          Fusce ipsum orci rhoncus aliporttitor integer platea placerat.
+          Nossa IA Jurídica automatiza a elaboração de petições e a análise detalhada de documentos,
+          proporcionando precisão e agilidade nos processos legais. Simplifique a gestão dos seus casos e foque no que é realmente importante.
         </p>
         <div className='flex flex-row flex-wrap sm:mt-10 mt-6'>
           <img
             src={apple}
-            alt='app-store'
+            alt='Acesse via App Store'
             className='w-[128px] h-[42px] object-contain mr-5 cursor-pointer'
           />
           <img
             src={google}
-            alt='google-play'
+            alt='Acesse via Google Play'
             className='w-[128px] h-[42px] object-contain cursor-pointer'
           />
         </div>
